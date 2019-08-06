50318c1c60567dfd5633e707c9ae361f47a11acf
Andrew Mark <codcore@gmail.com>
2019-08-06 17:18:37 +0300

    Add toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
-
+  <v-app>
+    <v-navigation-drawer app temporary></v-navigation-drawer>
+    <div>
+      <v-toolbar app dark>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>Ad spot</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn text>Link 1</v-btn>
+          <v-btn text>Link 2</v-btn>
+          <v-btn text>Link 3</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </div>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 
-export default {
+  export default {
 
-}
+  }
 </script>
